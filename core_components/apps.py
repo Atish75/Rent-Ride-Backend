81102dca434f9ca086615f167180fb2f3be0a599
theguy75 <empiretheguy75@gmail.com>
2026-09-01 09:21:33 +0530

    Remove mistakenly upload files

--- core_components/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CoreComponentsConfig(AppConfig):
-    name = 'core_components'
--- core_components/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import re_path
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(r'ws/location/(?P<car_id>\d+)/$', consumers.LocationConsumer.as_asgi()),
-]
